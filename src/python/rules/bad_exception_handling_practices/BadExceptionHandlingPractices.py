e313a6b6c0f3c1a5814aec54f63b5cfd7a123fc5
Sai Varun Teja Mudumba <samudumb@amazon.com>
2021-12-29 15:33:06 -0800

    Add examples of python/code-injection@v1.0, python/code-readability@v1.0, python/cross-site-request-forgery@v1.0, python/cross-site-scripting@v1.0, python/default-argument-mutable-objects@v1.0, python/deprecated-method@v1.0, python/clear-text-credentials@v1.0, python/catch-and-rethrow-exception@v1.0, python/bad-exception-handling-practices@v1.0, python/aws-unchecked-batch-failures@v1.0

---
+++ src/python/rules/bad_exception_handling_practices/BadExceptionHandlingPractices.py
@@ -0,0 +1,14 @@
+# {fact rule=bad-exception-handling-practices@v1.0 defects=1}
+def exception_handling_non_compliant(parameter):
+    if not isinstance(parameter, int):
+        # Noncompliant: a generic exception is thrown.
+        raise Exception("param should be an integer")
+# {/fact}
+
+
+# {fact rule=bad-exception-handling-practices@v1.0 defects=0}
+def exception_handling_compliant(parameter):
+    if not isinstance(parameter, int):
+        # Compliant: specific exception is thrown.
+        raise TypeError("param should be an integer")
+# {/fact}
