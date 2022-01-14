f8c8c022b966ead15d61ac6cb72aefdf17b05f12
Mallika Gadde <mgadde@amazon.com>
2022-01-14 14:57:46 -0500

    Changes to exclude complaint example for weak-obfuscation-of-request@v1.0

--- src/python/rules/weak_obfuscation_of_request/weak_obfuscation_of_request.py
+++ src/python/rules/weak_obfuscation_of_request/weak_obfuscation_of_request.py
@@ -6,19 +6,8 @@ def http_request_noncompliant(username, password, url):
     import urllib3 as urllib3
     from base64 import b64encode
     userpass = "%s:%s" % (username, password)
-    # Noncompliant: weak encoding used for basic authentication.
+    # Noncompliant: weak encoding used in HTTP Basic Authentication.
     authorization = b64encode(str.encode(userpass)).decode("utf-8")
-    headers = {'Authorization': 'Basic %s' % authorization}
-    urllib3.disable_warnings()
-    http = urllib3.PoolManager()
-    response = http.request('GET', url, headers=headers)
-# {/fact}
-
-
-# {fact rule=weak-obfuscation-of-request@v1.0 defects=0}
-def http_request_compliant(authorization, url):
-    import urllib3 as urllib3
-    # Compliant: no weak encoding used for basic authentication.
     headers = {'Authorization': 'Basic %s' % authorization}
     urllib3.disable_warnings()
     http = urllib3.PoolManager()
