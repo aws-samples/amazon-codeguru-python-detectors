c576615a708cfaa88e0242c5f0103b731156a7d4
Sai Varun Teja Mudumba <samudumb@amazon.com>
2022-01-14 16:07:58 -0800

    Add usual RMID

--- src/python/rules/naive_datatime_time_zone_issues/naive_datatime_time_zone_issues.py
+++ src/python/rules/naive_datatime_time_zone_issues/naive_datatime_time_zone_issues.py
@@ -1,16 +1,16 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  SPDX-License-Identifier: Apache-2.0
 
-# {fact rule=naive-datetime-time-zone-issues@v1.0 defects=1}
+# {fact rule=naive-datatime-time-zone-issues@v1.0 defects=1}
 def datetime_noncompliant():
-    from datetime import datetime, timezone
+    from datetime import datetime
     # Noncompliant: datetime method does not specify timezone,
     # resulting in time zone related issues.
     return datetime.now()
 # {/fact}
 
 
-# {fact rule=naive-datetime-time-zone-issues@v1.0 defects=0}
+# {fact rule=naive-datatime-time-zone-issues@v1.0 defects=0}
 def datetime_compliant():
     from datetime import datetime, timezone
     # Compliant: datetime method specifies the time zone,
