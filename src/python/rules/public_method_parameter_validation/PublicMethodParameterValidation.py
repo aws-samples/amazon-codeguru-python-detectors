8288496e26ede7bddd071d13f3378ecd775679d2
Sai Varun Teja Mudumba <samudumb@amazon.com>
2022-01-18 14:35:30 -0800

    Fix typos

--- src/python/rules/public_method_parameter_validation/PublicMethodParameterValidation.py
+++ src/python/rules/public_method_parameter_validation/PublicMethodParameterValidation.py
@@ -1,9 +1,11 @@
 # {fact rule=public-method-parameter-validation@v1.0 defects=1}
-def login_non_compliant(username, password):
+def login_noncompliant(username, password):
     # Noncompliant: not all public method parameters are validated for
     # nullness,unexpected values, and malicious values.
-    if password is not None and len(password) >=8:
+    if password is not None and len(password) >= 8:
         login(username, password)
+
+
 # {/fact}
 
 
@@ -14,7 +16,12 @@ def login_compliant(username, password):
     # Compliant: all the public method parameters are validated for nullness,
     # unexpected values, and malicious values.
     if username is not None and password is not None \
-            and username.isalnum() and len(passoword) >=8 \
+            and username.isalnum() and len(password) >= 8 \
             and re.search(regex, username):
         login(username, password)
+
+
 # {/fact}:
+
+def login(a, b):
+    return
