67e65a352f3f0f52c58976e3336488645f458a23
Mallika Gadde <mgadde@amazon.com>
2022-01-19 18:41:58 -0500

    Changes to address pr comments

--- src/python/rules/os_command_injection/os_command_injection.py
+++ src/python/rules/os_command_injection/os_command_injection.py
@@ -2,23 +2,27 @@
 #  SPDX-License-Identifier: Apache-2.0
 
 # {fact rule=os-command-injection@v1.0 defects=1}
-def os_command_noncompliant():
-    import subprocess
+def exec_command_noncompliant():
+    from paramiko import client
     from flask import request
-    # Noncompliant: address argument is not sanitized.
     address = request.args.get("address")
     cmd = "ping -c 1 %s" % address
-    subprocess.run(cmd, shell=True)
+    client = client.SSHClient()
+    client.connect("ssh.samplehost.com")
+    # Noncompliant: address argument is not sanitized.
+    client.exec_command(cmd)
 # {/fact}
 
 
 # {fact rule=os-command-injection@v1.0 defects=0}
-def os_command_compliant():
-    import shlex
-    import subprocess
+def exec_command_compliant():
+    from paramiko import client
     from flask import request
+    address = request.args.get("address")
     # Compliant: address argument is sanitized (shell-escaped).
     address = shlex.quote(request.args.get("address"))
     cmd = "ping -c 1 %s" % address
-    subprocess.run(cmd, shell=True)
+    client = client.SSHClient()
+    client.connect("ssh.samplehost.com")
+    client.exec_command(cmd)
 # {/fact}
