cc9810eed4bb068f0599d516a31f1267a48b16c4
Subarno Banerjee <98043512+awsubarno@users.noreply.github.com>
2022-01-20 15:11:16 -0800

    Add examples of python/log-injection@v1.0 (#26)

---
+++ src/python/rules/log_injection/log_injection.py
@@ -0,0 +1,22 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  SPDX-License-Identifier: Apache-2.0
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+# {fact rule=log-injection@v1.0 defects=1}
+def logging_noncompliant():
+    filename = input("Enter a filename: ")
+    # Noncompliant: unsanitized input is logged.
+    logger.info("Processing %s", filename)
+# {/fact}
+
+
+# {fact rule=log-injection@v1.0 defects=0}
+def logging_compliant():
+    filename = input("Enter a filename: ")
+    if re.match(r'^[\w_ -\.]+$', filename):
+        # Compliant: input is validated before logging.
+        logger.info("Processing %s", filename)
+# {/fact}
