c1288727fa6f05b9ae53e5f106c7113bdd3e5db3
Prajakta Ingle <prajaki@amazon.com>
2022-06-06 12:27:05 +0530

    Addressed the review comments for integer-overflow@v1.0

--- src/python/detectors/integer_overflow/integer_overflow.py
+++ src/python/detectors/integer_overflow/integer_overflow.py
@@ -1,8 +1,8 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  SPDX-License-Identifier: Apache-2.0
 
-# {fact rule=integer-overflow@v1.0 defects=1}
 import numpy as np
+# {fact rule=integer-overflow@v1.0 defects=1}
 def integer_overflow_noncompliant():
     # Noncompliant: Number larger than limit of the datatype is stored.
     arr = np.array([[100000000]], dtype=np.int8)
