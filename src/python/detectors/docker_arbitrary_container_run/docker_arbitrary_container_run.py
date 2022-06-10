3b5b29baefee21eaabf63219e7ed4ba876a5fd16
Prajakta Ingle <prajaki@amazon.com>
2022-06-10 18:39:16 +0530

    Addressed comments for docker-arbitrary-container-run@v1.0

--- src/python/detectors/docker_arbitrary_container_run/docker_arbitrary_container_run.py
+++ src/python/detectors/docker_arbitrary_container_run/docker_arbitrary_container_run.py
@@ -1,11 +1,14 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  SPDX-License-Identifier: Apache-2.0
 
+from flask import request
+import html
+import docker
+
+
 # {fact rule=docker-arbitrary-container-run@v1.0 defects=1}
 @app.route('/someUrl')
-def docker_arbitrary_container_run_noncompliant(request):
-    import docker
-    from flask import request
+def docker_arbitrary_container_run_noncompliant():
     client = docker.from_env()
     img = request.args.get("image")
     # Noncompliant: Unsanitised user input is passed to `run`.
@@ -15,8 +18,7 @@ def docker_arbitrary_container_run_noncompliant(request):
 
 # {fact rule=docker-arbitrary-container-run@v1.0 defects=0}
 @app.route('/someUrl')
-def docker_arbitrary_container_run_compliant(request):
-    import html
+def docker_arbitrary_container_run_compliant():
     img = html.escape(request.args.get("image"))
     # Compliant: User input is sanitised using `html.escape`.
     client.containers.run(img, 'echo hello world')
