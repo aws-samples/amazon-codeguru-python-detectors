a5aae614f1025895e62d4ea8ab0ebe73b76d9cb1
Prajakta Ingle <prajaki@amazon.com>
2022-06-24 11:17:56 +0530

    Addressed comments for hardcoded-ip-address@v1.0

--- src/python/detectors/hardcoded_ip_address/hardcoded_ip_address.py
+++ src/python/detectors/hardcoded_ip_address/hardcoded_ip_address.py
@@ -1,9 +1,11 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  SPDX-License-Identifier: Apache-2.0
 
+from socket import AF_INET, SOCK_STREAM, socket
+
+
 # {fact rule=hardcoded-ip-address@v1.0 defects=1}
 def hardcoded_ip_address_noncompliant():
-    from socket import AF_INET, SOCK_STREAM, socket
     sock = socket(AF_INET, SOCK_STREAM)
     # Noncompliant: IP address is hardcoded.
     sock.bind(('193.168.14.31', 80))
@@ -12,7 +14,6 @@ def hardcoded_ip_address_noncompliant():
 
 # {fact rule=hardcoded-ip-address@v1.0 defects=0}
 def hardcoded_ip_address_compliant(ip_add=None):
-    from socket import AF_INET, SOCK_STREAM, socket
     sock = socket(AF_INET, SOCK_STREAM)
     # Compliant: IP address is not hardcoded.
     sock.bind((ip_add, 5080))
