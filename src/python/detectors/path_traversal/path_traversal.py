cc1dc2dbe6fa1d906e601b046210cd6d3975d573
Prajakta Ingle <prajaki@amazon.com>
2022-08-09 10:50:56 +0530

    Corrected the code example for python/path-traversal@v1.0

--- src/python/detectors/path_traversal/path_traversal.py
+++ src/python/detectors/path_traversal/path_traversal.py
@@ -14,9 +14,11 @@ def verify_file_path_noncompliant():
 # {fact rule=path-traversal@v1.0 defects=0}
 def verify_file_path_compliant():
     from flask import request
+    base_path = "/var/data/images/"
     file_path = request.args["file"]
+    allowed_path = ["'example_path1', 'example_path2"]
     # Compliant: user input file path is sanitized.
-    file_path = file_path.replace("..", "")
-    file = open(file_path)
-    file.close()
+    if file_path in allowed_path:
+        file = open(base_path + file_path)
+        file.close()
 # {/fact}
