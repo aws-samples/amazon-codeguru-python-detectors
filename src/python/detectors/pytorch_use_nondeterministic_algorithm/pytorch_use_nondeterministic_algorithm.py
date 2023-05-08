2de48789c6f218029f221c413a57caf78b376084
Sujay Narsale <narsale@amazon.com>
2023-05-08 12:28:50 -0700

    Update pytorch_use_nondeterministic_algorithm.py

--- src/python/detectors/pytorch_use_nondeterministic_algorithm/pytorch_use_nondeterministic_algorithm.py
+++ src/python/detectors/pytorch_use_nondeterministic_algorithm/pytorch_use_nondeterministic_algorithm.py
@@ -1,10 +1,9 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  SPDX-License-Identifier: Apache-2.0
 # {fact rule=pytorch-use-nondeterministic-algorithm@v1.0 defects=1}
-
 def pytorch_use_nondeterministic_algorithm_noncompliant():
     import torch
-    # Noncompliant: doesn't use deterministic algorithms.
+    # Noncompliant: `torch.bmm` doesn't use deterministic algorithms by default.
     torch.bmm(torch.randn(2, 2, 2).to_sparse().cuda(),
               torch.randn(2, 2, 2).cuda())
 # {/fact}
@@ -13,7 +12,7 @@ def pytorch_use_nondeterministic_algorithm_noncompliant():
 # {fact rule=pytorch-use-nondeterministic-algorithm@v1.0 defects=0}
 def pytorch_use_nondeterministic_algorithm_compliant():
     import torch
-    # Compliant: uses deterministic algorithms.
+    # Compliant: configure `torch.bmm` to use deterministic algorithms.
     torch.use_deterministic_algorithms(True)
     torch.bmm(torch.randn(2, 2, 2).to_sparse().cuda(),
               torch.randn(2, 2, 2).cuda())
