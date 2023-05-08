ad5ac999befcf6fbe35932327c2054da29f56e06
Sujay Narsale <narsale@amazon.com>
2023-05-08 13:14:10 -0700

    Update pytorch_sigmoid_before_bceloss.py

--- src/python/detectors/pytorch_sigmoid_before_bceloss/pytorch_sigmoid_before_bceloss.py
+++ src/python/detectors/pytorch_sigmoid_before_bceloss/pytorch_sigmoid_before_bceloss.py
@@ -5,7 +5,7 @@
 def pytorch_sigmoid_before_bceloss_noncompliant():
     import torch
     import torch.nn as nn
-    # Noncompliant: `Sigmoid` layer followed by `BCELoss` 
+    # Noncompliant: `Sigmoid` layer followed by `BCELoss`
     # is not numerically robust.
     m = nn.Sigmoid()
     loss = nn.BCELoss()
@@ -23,7 +23,7 @@ def pytorch_sigmoid_before_bceloss_compliant():
     import torch
     import torch.nn as nn
     # Compliant: `BCEWithLogitsLoss` function integrates a `Sigmoid`
-    # layer and the `BCELoss` into one class 
+    # layer and the `BCELoss` into one class
     # and is numerically robust.
     loss = nn.BCEWithLogitsLoss()
 
