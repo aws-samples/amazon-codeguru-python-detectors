77b491df2539fe3d1eaae1b213068b7046f66b41
ankit-amazon <125257518+ankit-amazon@users.noreply.github.com>
2023-05-09 21:21:38 +0530

    Update pytorch_miss_call_to_eval.py

--- src/python/detectors/pytorch_miss_call_to_eval/pytorch_miss_call_to_eval.py
+++ src/python/detectors/pytorch_miss_call_to_eval/pytorch_miss_call_to_eval.py
@@ -22,7 +22,7 @@ def pytorch_miss_call_to_eval_compliant(model):
     model.load_state_dict(torch.load("model.pth"))
     classes = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
                "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
-    # Compliant: `eval()` is called before evaluating the model. 
+    # Compliant: `eval()` is called before evaluating the model.
     model.eval()
     x, y = test_data[0][0], test_data[0][1]
     with torch.no_grad():
