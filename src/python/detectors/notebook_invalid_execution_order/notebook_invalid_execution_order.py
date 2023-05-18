703ece0b4cd712be1d9e943377d4c2f54d07f42c
Ankit Patil <ankipatd@amazon.com>
2023-05-18 14:41:22 +0530

    Remove ipynb and add py files for python/notebook-invalid-execution-order

--- src/python/detectors/notebook_invalid_execution_order/notebook_invalid_execution_order.py
+++ src/python/detectors/notebook_invalid_execution_order/notebook_invalid_execution_order.py
@@ -2,7 +2,7 @@
 #  SPDX-License-Identifier: Apache-2.0
 
 # {fact rule=notebook-invalid-execution-order@v1.0 defects=1}
-# Compliant: execution order of cells is not linear. When executing
+# Noncompliant: execution order of cells is not linear. When executing
 # cells in linear order, variables `x` and `y` are undefined before use.
 # —— Code Cell 1, Execution Count 2 —— #
 sum = x + y
@@ -11,6 +11,20 @@ exp = x ** y
 # —— Code Cell 2, Execution Count 1 —— #
 x = 12
 y = 13
+# —— Code Cell 3, Execution Count 3 —— #
+print(sum, product, exp)
+# {/fact}
+
+
+# {fact rule=notebook-invalid-execution-order@v1.0 defects=0}
+# Compliant: execution order of cells is linear.
+# —— Code Cell 1, Execution Count 1 —— #
+x = 12
+y = 13
+# —— Code Cell 2, Execution Count 2 —— #
+sum = x + y
+product = x * y
+exp = x ** y
 # —— Code Cell 3, Execution Count 3 —— #
 print(sum, product, exp)
 # {/fact}
